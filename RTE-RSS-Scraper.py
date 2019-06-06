70f64727393af5dddfd3dffaf503baedb20842b6
Aidan Dennehy <duinneacha@gmail.com>
2019-06-06 15:37:44 +0100

    Added mysql on local host and improved parsing

--- RTE-RSS-Scraper.py
+++ RTE-RSS-Scraper.py
@@ -1,8 +1,18 @@
 # import urllib.request
 
 import requests
+import mysql.connector
 from bs4 import BeautifulSoup
 from csv import writer
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  passwd="",
+  database="nimbus-admin"
+)
+
+
 
 req = requests.get(
     'https://www.rte.ie/feeds/rss/?index=/news/business/')
@@ -17,6 +27,41 @@ soup = BeautifulSoup(req.text, 'html.parser')
 #     print(num+1)
 #     print(item)
 
+print("ADDDDD")
+print(soup)
+print("ADDDDD")
 
-for item in soup.select('.item'):
-    print(item.get_text())
+
+for item in soup.findAll('item'):
+   print("AD")
+   # print(item)
+   print(item.category.get_text())
+   print(item.title.get_text())
+   print(item.description.get_text())
+   print(item.guid.get_text())
+   thumb = item.find("media:content")
+   print(thumb)
+   print(thumb["url"])
+   # thumbUrl = thumb.find("url") 
+   # print(thumbUrl)
+
+
+
+print("mydb:-")
+print(mydb)
+
+
+mycursor = mydb.cursor()
+
+mycursor.execute("SHOW TABLES")
+
+for x in mycursor:
+  print(x)
+
+
+mycursor.execute("SELECT cl_id, cl_name FROM tbl_contacts")
+
+myresult = mycursor.fetchall()
+
+for x in myresult:
+  print(x)
